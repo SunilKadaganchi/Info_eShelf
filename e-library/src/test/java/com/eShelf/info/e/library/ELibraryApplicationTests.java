bdb71a274f6a60ccb9c1f3596a5082260f860324
SunilKadaganchi <sunilkadaganchi071@gmail.com>
2024-09-18 17:16:38 +0530

    initial commit

---
+++ e-library/src/test/java/com/eShelf/info/e/library/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eShelf.info.e.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
